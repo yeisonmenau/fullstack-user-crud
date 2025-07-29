345b31b1ccacc0d2d15280f6c16a925cb1f90f42
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-29 12:12:07 -0500

    Crear entidad Usuario en capa de dominio

---
+++ app/back/src/main/java/com/yeisonmenau/back/domain/model/Usuario.java
@@ -0,0 +1,10 @@
+package com.yeisonmenau.back.domain.model;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private Long cedula;
+    private String nombre;
+    private String correo;
+    private LocalDate fechaNacimiento;
+}
